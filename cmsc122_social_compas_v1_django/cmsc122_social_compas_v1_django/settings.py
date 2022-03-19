f81474c6fadbaf39d41d59bc48d7c43605ba6f55
Numbers00 <57945780+Numbers00@users.noreply.github.com>
2022-03-19 21:46:51 +0800

    removed SMTP temporarily

--- cmsc122_social_compas_v1_django/cmsc122_social_compas_v1_django/settings.py
+++ cmsc122_social_compas_v1_django/cmsc122_social_compas_v1_django/settings.py
@@ -159,13 +159,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-# EMAIL_HOST_USER = 'kylemariangelo@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Imaginebreaker'
 
 # if os.getcwd() == '/app':
 #     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PHOTO')
