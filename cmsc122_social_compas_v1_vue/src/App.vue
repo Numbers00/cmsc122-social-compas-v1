3fd4909d0964788201268e6a351f75f8c407f6c8
Numbers_00 <kylemariangelo@gmail.com>
2022-03-20 10:25:14 +0800

    removed HomeView and AboutView, added preliminary FLAMESView

--- cmsc122_social_compas_v1_vue/src/App.vue
+++ cmsc122_social_compas_v1_vue/src/App.vue
@@ -31,10 +31,6 @@
         </div>
       </div>
     </nav>
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav> -->
     <router-view/>
   </div>
 </template>
---
+++ cmsc122_social_compas_v1_vue/src/views/FLAMESView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flames-body container">
+    this is flames
+  </div>
+</template>
