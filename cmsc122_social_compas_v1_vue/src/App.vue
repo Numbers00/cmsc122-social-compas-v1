9b5b2a3dff982ec1ed23ab84a651dbd58750421a
Numbers_00 <kylemariangelo@gmail.com>
2022-03-20 10:16:48 +0800

    added navbar

--- cmsc122_social_compas_v1_vue/src/App.vue
+++ cmsc122_social_compas_v1_vue/src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class='container'>
+    <nav class="navbar navbar-expand-lg navbar-dark mt-5 rounded" style="background-color: #e11584;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">COMPAS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Database</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Interact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">FLAMES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Horoscope</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
